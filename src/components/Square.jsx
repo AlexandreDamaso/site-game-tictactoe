79ce4ef69184f35e1b90d49779f6956b54a0d070
Alexandre Damaso <alexandredamasocosta@gmail.com>
2025-10-28 18:03:05 -0300

    feito ate Verificar Vencedor

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Square.css'
 
-function Square() {
+function Square({value, onClick}) {
+   let style = null;
+
+   if (value == 'X') 
+      style = 'square x'
+   else if (value == 'O')
+      style = 'square o'
+   else
+      style = 'square'
+
    return (
-      <div>Square</div>
+      <div>
+         <button onClick={onClick} className={style}>
+            {value}
+         </button>
+      </div>
    )
 }
 
